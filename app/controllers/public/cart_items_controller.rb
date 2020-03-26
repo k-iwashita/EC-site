43b2ffa05f0d9daa8283f31da4ed8e0b8fd67487
k-iwashita <k.iwashita.gude@gmail.com>
2020-03-26 09:17:38 +0900

    カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,24 +1,35 @@
 class Public::CartItemsController < ApplicationController
   def index
-    
+    @cart_items = current_end_user.cart_items
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.amount = params[:cart_item][:amount]
+    cart_item.save
+    redirect_to cart_items_path
   end
 
   def destroy
-
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
-
+    current_end_user.cart_items.destroy_all
+    redirect_to root_path
   end
 
   def create
     cart_item = CartItem.new(amount: params[:cart_item][:amount])
     cart_item.end_user_id = current_end_user.id
     cart_item.item_id = params[:cart_item][:id]
-    cart_item.save
-    redirect_to cart_items_path
+    if cart_item.save
+      redirect_to cart_items_path
+    else
+      @item = Item.find(params[:cart_item][:id])
+      render  item_path(@item)
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  has_many :cart_items, dependent: :destroy
 
   enum sale_status: { 販売中: 0, 販売中止: 1 }
 end
